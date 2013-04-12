75d5b836618988bdf51963e1a2cc6deedeb80a69
Enrico Cavalli <enrico.cavalli@gmail.com>
2013-04-12 12:03:18 +0200

    download GeoIPv6.dat and ignore *.dat files

--- update-geoipdb.sh
+++ update-geoipdb.sh
@@ -2,3 +2,6 @@
 
 wget http://download.maxmind.com/download/geoip/database/asnum/GeoIPASNumv6.dat.gz
 gunzip -f GeoIPASNumv6.dat.gz
+
+wget http://geolite.maxmind.com/download/geoip/database/GeoIPv6.dat.gz
+gunzip -f GeoIPv6.dat
