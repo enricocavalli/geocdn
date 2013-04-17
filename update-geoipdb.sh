59d93cd978ca20ab2404838b264217a2a086aab4
Enrico Cavalli <enrico.cavalli@gmail.com>
2013-04-17 09:47:10 +0200

    trivial bug

--- update-geoipdb.sh
+++ update-geoipdb.sh
@@ -4,4 +4,4 @@ wget http://download.maxmind.com/download/geoip/database/asnum/GeoIPASNumv6.dat.
 gunzip -f GeoIPASNumv6.dat.gz
 
 wget http://geolite.maxmind.com/download/geoip/database/GeoIPv6.dat.gz
-gunzip -f GeoIPv6.dat
+gunzip -f GeoIPv6.dat.gz
